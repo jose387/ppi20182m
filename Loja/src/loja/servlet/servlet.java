3b7969491aa90dc1fde79e18d67dc4ac027455ba
20151074010387 <20151074010387@MC266468.ifrn.local>
2018-09-17 09:29:20 -0300

    Adicionando projetos no repositório

---
+++ Loja/src/loja/servlet/servlet.java
@@ -0,0 +1,21 @@
+package loja.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class servlet extends HttpServlet {
+	
+	@Override
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		String nome = request.getParameter("nome");
+		String endereco = request.getParameter("endereco");
+		String CPF = request.getParameter("CPF");
+		String RG = request.getParameter("RG");
+	}
+
+}
